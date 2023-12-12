33fdeb98cba6c0f93a5eec13f75f03cac7becef4
sangukO <woqls1972@gmail.com>
2023-12-12 14:02:08 +0900

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <main>
+    123123
+    <span class="font-extrabold">123</span>
+  </main>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { loadEnv, defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
+
+const resolveAlias = Object.fromEntries(
+  Object.entries({
+    '@components': './src/components',
+    '@pages': './src/pages',
+    '@router': './src/router',
+    '@stores': './src/stores',
+    '@utils': './src/utils',
+    '@api': './src/api',
+    '@assets': './src/assets',
+    '@dist': './dist'
+    //   '@composable': './src/composable',
+    //   '@cstore': './src/composable/store',
+    //   '@directive': './src/directive',
+    //   '@plugins': './src/plugins',
+    //   '@postcss': './src/assets/postcss',
+  }).map(([key, value]) => [key, path.resolve(__dirname, value)])
+)
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.join(__dirname, './env'), '')
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: resolveAlias
+    },
+    envPrefix: 'VUE_',
+    envDir: './env',
+    server: {
+      host: true,
+      proxy: {}
+    }
+  }
+})
