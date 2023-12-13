c7eb7131358f75f505fb5afd2a99ef489f205950
sangukO <woqls1972@gmail.com>
2023-12-13 15:15:39 +0900

    feat : 다크모드 초안 완성

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,42 @@
-<script setup></script>
-
 <template>
   <main>
-    123123
-    <span class="font-extrabold">123</span>
+    <div class="w-full h-full flex flex-row-reverse py-10 px-10">
+      <input
+        type="checkbox"
+        role="switch"
+        class="toggle-theme"
+        id="toggle-theme"
+        v-model="negatedDarkMode"
+      />
+    </div>
   </main>
 </template>
+<script setup>
+import { ref, onBeforeMount, computed, watch } from 'vue'
+const darkMode = ref(true)
+
+const negatedDarkMode = computed({
+  get: () => !darkMode.value,
+  set: (value) => {
+    darkMode.value = !value
+    savePreference(darkMode.value)
+    document.documentElement.setAttribute('color-theme', darkMode.value ? 'dark' : 'light')
+  }
+})
+
+const getInitialTheme = () => {
+  if (localStorage.getItem('isDarkMode') === 'false') {
+    return false
+  }
+  return true
+}
+
+const savePreference = (isDarkMode) => {
+  localStorage.setItem('isDarkMode', isDarkMode)
+}
+
+onBeforeMount(() => {
+  darkMode.value = getInitialTheme()
+  document.documentElement.setAttribute('color-theme', darkMode.value ? 'dark' : 'light')
+})
+</script>
