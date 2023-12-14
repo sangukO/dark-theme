4d08c168aa51256f1af126bbc3c67fdc083649a4
sangukO <woqls1972@gmail.com>
2023-12-14 18:01:43 +0900

    feat : 버튼 추가 및 인사 멘트 변경

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,59 +1,90 @@
 <template>
-  <main>
-    <div class="w-full h-full">
-      <div class="flex justify-between flex-row-reverse py-10 px-10">
-        <input
-          type="checkbox"
-          role="switch"
-          class="toggle-theme"
-          id="toggle-theme"
-          v-model="negatedDarkMode"
-        />
-        <p>
-          {{ formattedTime }}
-        </p>
-      </div>
+  <main class="w-[100vw] h-[100vh]">
+    <div class="flex justify-between flex-row-reverse py-10 px-10">
+      <input
+        type="checkbox"
+        role="switch"
+        class="toggle-theme"
+        id="toggle-theme"
+        v-model="negatedDarkMode"
+      />
+      <p class="flex items-center mb-0 !text-3xl">
+        {{ formattedTime }}
+      </p>
     </div>
-    <div class="flex justify-center items-center flex-col">
-      <div class="nes-container with-title w-[800px]" :class="darkMode ? 'is-dark' : ''">
-        <span class="title !text-4xl !mt-[-55px]">음식 추천 봇</span>
-        <p class="mt-5">환영합니다. 좋은 {{ wordVariable }}입니다.</p>
-        <p>저에게 오세요.</p>
-        <p>음식 추천을 해드리겠습니다.</p>
+
+    <div class="flex items-center flex-col h-[calc(100vh-145px)] pt-40">
+      <div class="nes-container with-title w-[40%]" :class="darkMode ? 'is-dark' : ''">
+        <span class="title !text-4xl !mt-[-45px]">봇</span>
+        <!-- <p class="mt-5 !text-2xl sentence1">안녕하세요. 좋은 {{ wordVariable.time }}입니다.</p> -->
+        <p class="mt-5 !text-2xl">{{ sentence1 }}</p>
+        <p class="!text-2xl sentence2">저에게 오세요.</p>
+        <p class="!text-2xl sentence3">{{ wordVariable.food }} 추천을 해드리겠습니다.</p>
       </div>
+      <transition name="pick">
+        <div v-if="picked === ''" class="pt-8 flex gap-x-20">
+          <label>
+            <input
+              type="radio"
+              class="nes-radio is-dark"
+              name="answer-dark"
+              value="Y"
+              v-model="picked"
+              :disabled="picked != ''"
+            />
+            <span>좋아</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              class="nes-radio is-dark"
+              name="answer-dark"
+              value="N"
+              v-model="picked"
+              :disabled="picked != ''"
+            />
+            <span>싫어</span>
+          </label>
+        </div>
+      </transition>
     </div>
   </main>
 </template>
 <script setup>
-import { ref, onBeforeMount, computed, watch, onMounted } from 'vue'
+import { ref, onBeforeMount, computed, watch, onMounted, nextTick } from 'vue'
 import { useDateFormat, useNow, useTimestamp } from '@vueuse/core'
 const darkMode = ref(true)
 const todayMidnight = ref(new Date())
 const formattedTime = useDateFormat(useNow(), 'YYYY-MM-DD HH:mm:ss')
 const now = useTimestamp()
 const nowHMS = ref(now.value - todayMidnight.value.setHours(0, 0, 0, 0))
+const picked = ref('')
+const sentence1 = ref('안녕하세요')
 
 const updateWordVariable = () => {
   if (nowHMS.value > 0 && nowHMS.value <= 7200000) {
     // 0~3
-    return '밤'
+    return { time: '밤', food: '야식' }
   } else if (nowHMS.value > 7200000 && nowHMS.value <= 21600000) {
     // 3~6
-    return '새벽'
+    return { time: '새벽', food: '야식' }
   } else if (nowHMS.value > 21600000 && nowHMS.value <= 43200000) {
     // 6~12
-    return '아침'
-  } else if (nowHMS.value > 43200000 && nowHMS.value <= 64800000) {
-    // 12~18
-    return '낮'
-  } else if (nowHMS.value > 64800000 && nowHMS.value <= 75600000) {
-    // 18~21
-    return '저녁'
+    return { time: '아침', food: '아침 식사' }
+  } else if (nowHMS.value > 43200000 && nowHMS.value <= 50400000) {
+    // 12~14
+    return { time: '낮', food: '점심 식사' }
+  } else if (nowHMS.value > 50400000 && nowHMS.value <= 61200000) {
+    // 14~17
+    return { time: '오후', food: '간식' }
+  } else if (nowHMS.value > 61200000 && nowHMS.value <= 75600000) {
+    // 17~21
+    return { time: '저녁', food: '저녁 식사' }
   } else if (nowHMS.value > 75600000 && nowHMS.value <= 86400000) {
     // 21~0
-    return '밤'
+    return { time: '밤', food: '야식' }
   } else {
-    return '날'
+    return { time: '날', food: '음식' }
   }
 }
 
@@ -62,6 +93,12 @@ const wordVariable = ref(updateWordVariable())
 watch(now, () => {
   nowHMS.value = now.value - todayMidnight.value.setHours(0, 0, 0, 0)
   updateWordVariable()
+})
+
+watch(picked, async (to) => {
+  console.log(picked.value, to)
+  await nextTick()
+  sentence1.value = '감사합니다.'
 })
 
 const negatedDarkMode = computed({
@@ -89,3 +126,8 @@ onBeforeMount(() => {
   document.documentElement.setAttribute('color-theme', darkMode.value ? 'dark' : 'light')
 })
 </script>
+<style lang="postcss" scoped>
+.pick-leave-active {
+  transition-delay: 1500ms;
+}
+</style>
