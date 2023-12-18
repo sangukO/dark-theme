ca5b918acb0544a075cb7a1183c0df4c5470c881
sangukO <woqls1972@gmail.com>
2023-12-18 18:01:15 +0900

    docs : typed 라이브러리 추가, 작업중

--- src/components/pages/home/Conversation.vue
+++ src/components/pages/home/Conversation.vue
@@ -3,10 +3,12 @@
     class="nes-container with-title w-[80%] max-w-[1000px] min-h-[148px]"
     :class="isDarkMode ? 'is-dark' : ''"
   >
-    <span class="title !text-[3.2rem] !mt-[-4rem]">봇</span>
-    <p>{{ s1 }}</p>
-    <p>{{ s2 }}</p>
-    <p>{{ s3 }}</p>
+    <span class="title !text-[3.2rem] !mt-[-4rem]">음식 추천 봇</span>
+    <!-- <vue-typed-js :strings="['First text', 'Second Text']">
+      <p class="typing"></p>
+    </vue-typed-js> -->
+    <p class="s2">{{ s2 }}</p>
+    <p class="s3">{{ s3 }}</p>
   </div>
 </template>
 
@@ -17,6 +19,13 @@ defineProps({
   s3: String,
   isDarkMode: Boolean
 })
+
+// const typed = new Typed('#typed', {
+//   strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+//   typeSpeed: 40,
+//   backSpeed: 0,
+//   loop: true
+// })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="postcss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import router from './router'
 import '@assets/base.css'
 import '@assets/tailwind.css'
 import 'nes.css/css/nes.min.css'
+import VueTypedJs from 'vue-typed-js'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueTypedJs)
 
 app.mount('#app')
